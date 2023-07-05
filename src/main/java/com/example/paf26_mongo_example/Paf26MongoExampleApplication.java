595a021c27272ead0bff4528731e60db48a2d958
Yenleng <yenlengggggg@gmail.com>
2023-07-05 11:51:29 +0800

    initial commit

---
+++ src/main/java/com/example/paf26_mongo_example/Paf26MongoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.paf26_mongo_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paf26MongoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paf26MongoExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paf26_mongo_example/Paf26MongoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paf26_mongo_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paf26MongoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
